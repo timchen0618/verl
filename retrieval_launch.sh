fb0c365d5b9b4baaf73705ba3e29f9bc5d720cac
timchen0618 <tchen87618@gmail.com>
2026-05-13 16:12:13 -0400

    retrieval fix

--- retrieval_launch.sh
+++ retrieval_launch.sh
@@ -4,7 +4,7 @@ index_file=$file_path/e5_Flat.index
 corpus_file=$file_path/wiki-18.jsonl
 retriever_name=e5
 retriever_path=intfloat/e5-base-v2
-python search_r1/search/retrieval_server.py --index_path $index_file \
+python examples/sglang_multiturn/search_r1_like/local_dense_retriever/retrieval_server.py --index_path $index_file \
                                             --corpus_path $corpus_file \
                                             --topk 3 \
                                             --retriever_name $retriever_name \
